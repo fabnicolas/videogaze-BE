155f69edef1ac84bef88ec56fb106e5aaca73ca4
Finalgalaxy <finalgalaxy@gmail.com>
2018-03-23 16:39:48 +0100

    Added support for datetime with precision.

--- include/functions.php
+++ include/functions.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Utility functions class that can be used anywhere by including this file.
+ * 
+ * Handy for a lot of tasks that are repeated over time.
+ * 
+ * @author Fabio Crispino
+ */
+
+
 function enable_errors(){
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
@@ -60,8 +69,29 @@ function unique_random_string($length=16){
 		return bin2hex(openssl_random_pseudo_bytes($length));
 }
 
-function sql_datetime(){
-	return date("Y-m-d H:i:s");
+function sql_datetime($precision=0){
+	$timestamp_micro = microtime();
+    list($msec, $sec) = explode(' ', $timestamp_micro);
+    $msec = explode(".",$msec);
+    $msec = $msec[1];
+		$msec = substr($msec,0,$precision);
+		$append='';
+	if($precision>0) $append=".".$msec;
+	return date("Y-m-d H:i:s".$append);
+}
+
+function array_equals(array $array1, array $array2){
+	$are_equals=true;
+	if(count($array1) != count($array2)) return false;
+	else{
+		foreach($array1 as $key=>$value){
+			if(!isset($array2[$key]) || $array1[$key]!=$array2[$key]){
+				$are_equals=false;
+				break;
+			}
+		}
+	}
+	return $are_equals;
 }
 
 // Enable errors
